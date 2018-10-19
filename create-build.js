4345b2cb1df587fb24d91f7d58cb032b1d00f360
Josh Kalderimis <josh.kalderimis@gmail.com>
2018-10-19 14:27:13 -0700

    a first run at an action

---
+++ create-build.js
@@ -0,0 +1,28 @@
+const axios = require('axios')
+
+const repository = encodeURIComponent(process.env.GITHUB_REPOSITORY)
+
+var headers = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Travis-API-Version': '3',
+  'Authorization': `token ${process.env.TRAVIS_TOKEN}` 
+}
+
+var body = {
+  "request": {
+    "message": "This should be the commit message",
+    "branch":"master"
+  }
+}
+
+axios.post(`https://api.travis-ci.com/repo/${repository}/requests`, body, { headers: headers })
+.then((res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+  console.log(res)
+  process.exit()
+})
+.catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
